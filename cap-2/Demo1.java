45f3a411d2f14c2b09c561ab5cc747506e38daca
JordanDiaz19 <78715472+JordanDiaz19@users.noreply.github.com>
2021-02-24 15:50:12 -0600

    Add files via upload

---
+++ cap-2/Demo1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Libro.cap02.excepciones;
+
+public class Demo1
+{
+public static void main(String[] args)
+{
+try
+{
+System.out.println("Hola, chau !");
+return;
+}
+catch(Exception ex)
+{
+System.out.println("Entre al catch...");
+}
+finally
+{
+System.out.println("Esto sale siempre !");
+}
+}
+}
