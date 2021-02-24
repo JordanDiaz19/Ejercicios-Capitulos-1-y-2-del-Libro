6d57764bcaea9c2f9a3bce9d0719041b4c5609c0
JordanDiaz19 <78715472+JordanDiaz19@users.noreply.github.com>
2021-02-24 15:46:05 -0600

    Add files via upload

---
+++ cap-1/DemoCaracteres3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Libro.cap01;
+public class DemoCaracteres3
+{
+public static void main(String[] args)
+{
+char c;
+for( int i='A'; i<='Z'; i++ )
+{
+// para asignar un int en un char debemos "castear"
+c = (char) i;
+System.out.println(c);
+}
+}
+}
---
+++ cap-1/DemoCaracteresEspeciales.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Libro.cap01;
+
+public class DemoCaracteresEspeciales
+{
+public static void main(String[] args)
+{
+System.out.println("Esto\t es un \"TABULADOR\"");
+System.out.println("Esto es un\nBARRA\nENE\n\n :O)");
+System.out.println("La barra es asi: \\");
+}
+}
+
